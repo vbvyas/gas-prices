26369fc71b053a756a5bf7a1ffda53fd6f680b4b
Vinay Vyas <vbvyas@gmail.com>
2014-12-07 18:49:05 -0800

    Cleanup script

--- bin/script.js
+++ bin/script.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 var scraper = require('../lib/scraper');
-var request = require('request');
-var cheerio = require('cheerio');
 var fs = require('fs');
 var Path = require('path');
-var url = 'http://www.gaspricewatch.com/WA-washington/Seattle/gas-prices/page-1/2.htm';
 var file = Path.join(__dirname, '../data/prices.json');
-var prices = {};
 
 var prices = scraper.scrape('WA', 'Seattle');
 fs.writeFile(file, JSON.stringify(prices, null, 2), function(err) {
