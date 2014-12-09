ed770b37561d2180d9dd6c8dbe7fdcfbdad13543
Vinay Vyas <vbvyas@gmail.com>
2014-12-09 07:25:03 -0800

    Route to get prices

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 var Hapi = require('hapi');
+var scraper = require('./lib/scraper.js');
+
+var server = new Hapi.Server(3000);
+
+server.route({
+  method: 'GET',
+  path: '/{state}/{city}',
+  handler: function (req, res) {
+    var state = req.params.state;
+    var city = req.params.city;
+    var prices = scraper.scrape(state, city);
+    res(prices);
+  }
+});
+
+server.start(function () {
+  console.log('Hapi server started @', server.info.uri);
+})
